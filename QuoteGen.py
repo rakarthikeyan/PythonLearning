fd738329cf7b6fb89e2ebb3dd1b0b6c71ff843fc
Karthikeyan RA <karthixiomi@gmail.com>
2021-07-10 20:55:55 +0530

    10JUL2021_FINAL

---
+++ QuoteGen.py
@@ -0,0 +1,11 @@
+
+# QuoteGenerator - Every time its run, reads from a file and displays the quote
+
+import random
+
+f=open("quote.txt","r",encoding = 'utf-8')
+quote=f.readlines()
+len=len(quote)
+print("X"*25)
+print(random.choice(quote))
+print("X"*25)
