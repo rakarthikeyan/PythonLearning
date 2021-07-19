d7168ffe25faa74a3ce0ff1fdbecebb480b00a73
Karthikeyan RA <karthixiomi@gmail.com>
2021-07-19 20:19:09 +0530

    19JUL2021_Lambda

--- boto3_learning/boto3_listRunningEC2.py
+++ boto3_learning/boto3_listRunningEC2.py
@@ -5,18 +5,22 @@ mgt_console = boto3.Session(profile_name="devops")
 ec2 = mgt_console.client('ec2')
 regions = ec2.describe_regions()["Regions"]
 
-myMessage=""
+myMessage = ""
 for x in regions:
     # print(x['RegionName'])
     ec21 = mgt_console.client('ec2', region_name=x['RegionName'])
     response = ec21.describe_instance_status()['InstanceStatuses']
-    
+    # print(response)
     if len(response) > 0:
-        myMessage="Region: "+ x['RegionName'] + "\n"
+        myMessage = myMessage + "Region: " + x['RegionName'] + "\n"
         #print("Region:", x['RegionName'])
         for each_instance in response:
-            myMessage=myMessage + each_instance['InstanceId'] + '|' + each_instance['InstanceState']['Name'] + "\n"
-            #print(each_instance['InstanceId'], '|',
+            myMessage = myMessage + \
+                each_instance['InstanceId'] + '|' + \
+                each_instance['InstanceState']['Name'] + "\n"
+            # print(each_instance['InstanceId'], '|',
             #      each_instance['InstanceState']['Name'])
 
+if len(myMessage) == 0:
+    myMessage = "No Running Instances.."
 print(myMessage)
